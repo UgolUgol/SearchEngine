c20865ebba0f29d43052fb88690e5610b9cbfe85
Ugol Ugol <trubchenkon@gmail.com>
2019-03-24 21:59:50 +0300

    add makefile to index

---
+++ index/make_index.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <streambuf>
+#include <locale>
+#include <codecvt>
+#include <future>
+#include <cmath>
+#include <chrono>
+#include <tools.h>
+
+int main() {
+	std::vector<Tools::ArticleBox> bb;
+}
