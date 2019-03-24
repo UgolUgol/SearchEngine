ec1eca404c7d6ed3d95ad6acffd593812c9315f6
Ugol Ugol <trubchenkon@gmail.com>
2019-03-24 21:59:24 +0300

    add makefile to tokenizer

--- tokenizer/tokenizer.cpp
+++ tokenizer/tokenizer.cpp
@@ -8,52 +8,10 @@
 #include <future>
 #include <cmath>
 #include <chrono>
+#include <tools.h>
 
-
-using Tokens = std::vector<std::wstring>;
+using namespace Tools;
 const wchar_t* breakset = L"!#$%&()*+,./:;“„<=>?@[]^_{|}~»«\"\\—\n\t ";
-
-struct ArticleBox {
-	std::vector<std::wstring> names;
-	std::vector<std::wstring> urls;
-	std::vector<Tokens> tokens;
-};
-
-std::wstring readData(std::string filename) {
-
-	std::wifstream ifs(filename);
-	ifs.imbue(std::locale("ru_RU.utf8"));
-
-	ifs.seekg(0, std::ios::end);
-	size_t size = ifs.tellg();
-	std::wstring texts(size, L' ');
-	ifs.seekg(0, std::ios::beg);
-	
-	ifs.read(&texts[0], size);
-	ifs.close();
-
-	return texts;
-}
-
-
-void split_regex(Tokens& dest, const wchar_t* src, const wchar_t* breakset, size_t srcSize) {
-
-	const wchar_t* end = src + srcSize;
-	while(src != end) {
-		auto str = std::wcspbrk(src, breakset);
-		if(!str) {
-			dest.emplace_back(src);
-			break;
-		}
-
-		size_t diff = str - src;
-		if(diff) {
-			dest.emplace_back(src, diff);
-		}
-		src = str + 1;
-	}
-}
-
 
 bool isArticle(const std::wstring& token) {
 
