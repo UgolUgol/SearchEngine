e5400952de21efa05cbd33f4b24229cc7c0f135f
Nikita Trubchenko <nikita@vinogradov.komtex>
2019-04-05 16:23:09 +0300

    make_index particial read example

--- index/make_index.cpp
+++ index/make_index.cpp
@@ -8,28 +8,33 @@
 #include <cmath>
 #include <chrono>
 #include <tools.h>
+#include <cstring>
+#include <sstream>
+#include <unistd.h>
 
 int main() {
 	std::setlocale(LC_ALL, "ru_RU.utf8");
-	std::wifstream ifs("a");
+	std::wifstream ifs("tokens");
 	ifs.imbue(std::locale("ru_RU.utf8"));
-
 	while(!ifs.eof()) {
 		std::wstring info;
 		std::wstring text;
 
 		std::getline(ifs, info);
-		std::wistringstream ss(info);
 		
 		if(info.empty()) break;
-		size_t size;
+		
+		std::vector<std::wstring> articleInfo;
+		Tools::split_regex(articleInfo, info.c_str(), L"|", info.size());
+		
+		std::wistringstream ss(articleInfo[2]);
+		size_t size = 0;
 		ss >> size;
-
+		std::wcout<<articleInfo[0]<< " " << articleInfo[1] << " " << size<<std::endl;
 		text.assign(size - 1, L' ');
 		ifs.read(&text[0], size);
 		std::wcout<<text<<std::endl;
-		std::wcout<<"===="<<std::endl;
-		
+
 	}
 
 
