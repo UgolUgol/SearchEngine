e5982323ac1e5c684856af2ed9f93ca15b42d2b3
Nikita Trubchenko <nikita@vinogradov.komtex>
2019-04-26 17:29:25 +0300

    Trying memory mapping

--- boolean_search/make_search.cpp
+++ boolean_search/make_search.cpp
@@ -1,9 +1,14 @@
 #include <search.h>
 #include <iostream>
-
+#include <boost/interprocess/file_mapping.hpp>
+#include <filesystem>
 
 int main() {
-	Search::RequestHandler handler;
+	std::filesystem::path path = "../index/files/dict.bin";
+	auto filesize = std::filesystem::file_size(path);
+	std::cout<<filesize<<std::endl;
+/*	boost::interprocess::file_mapping m_file("../index/files/dict.bin", read);
+	boost::interprocess::mapped_region region(m_file, read);*/
 
 
 	return 0;
