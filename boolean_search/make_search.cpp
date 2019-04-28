aeb3162ab224fbbf33332d14f34dc2b2d541aec3
Ugol Ugol <trubchenkon@gmail.com>
2019-04-28 23:27:01 +0300

    index working testing

--- boolean_search/make_search.cpp
+++ boolean_search/make_search.cpp
@@ -1,14 +1,29 @@
 #include <search.h>
 #include <iostream>
 #include <boost/interprocess/file_mapping.hpp>
-#include <filesystem>
+#include <boost/interprocess/mapped_region.hpp>
+#include <functional>
 
 int main() {
-	std::filesystem::path path = "../index/files/dict.bin";
-	auto filesize = std::filesystem::file_size(path);
-	std::cout<<filesize<<std::endl;
-/*	boost::interprocess::file_mapping m_file("../index/files/dict.bin", read);
-	boost::interprocess::mapped_region region(m_file, read);*/
+	
+	boost::interprocess::file_mapping mfile("../index/files/dict.bin", boost::interprocess::read_write);
+	boost::interprocess::mapped_region region(mfile, boost::interprocess::read_write);
+
+	boost::interprocess::file_mapping mfile1("../index/files/coord.bin", boost::interprocess::read_write);
+	boost::interprocess::mapped_region region1(mfile1, boost::interprocess::read_write);
+
+	std::setlocale(LC_ALL, "ru_RU.utf8");
+	std::wstring s;
+	std::wcin >> s;
+	auto p = std::hash<std::wstring>{}(s);
+	auto addr = static_cast<size_t*>(region.get_address());
+	while(p != *addr) {
+		addr += 3;
+	}
+	auto offset = *(addr + 2);
+	std::cout << offset << std::endl;
+	auto coord_ptr = static_cast<size_t*>(region1.get_address());
+	std::cout<<*coord_ptr<<std::endl;
 
 
 	return 0;
