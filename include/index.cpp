ad46b7dcc94419024c059154bf37060517a2dbc4
Ugol Ugol <trubchenkon@gmail.com>
2019-05-02 11:15:04 +0300

    Add IndexTraits

--- include/index.cpp
+++ include/index.cpp
@@ -2,17 +2,15 @@
 #include <algorithm>
 
 
-template<typename NodeType, size_t DictNodeSize, size_t CoordNodeSize>
-Index<NodeType, DictNodeSize, CoordNodeSize>::Index(const char* dictFile, 
-										  const char* coordFile,
-										  const char* invCoordFile) : 
-										  dict(dictFile, boost::interprocess::read_write),
-										  coord(coordFile, boost::interprocess::read_write),
-										  invCoord(invCoordFile, boost::interprocess::read_write),
-										  mappedDict(dict, boost::interprocess::read_write),
-										  mappedCoord(coord, boost::interprocess::read_write),
-										  mappedInvCoord(invCoord, boost::interprocess::read_write) {
+template<typename IndexType>
+Index<IndexType>::Index(const char* dictFile, 
+					    const char* coordFile,
+						const char* invCoordFile) : 
+						dict(dictFile, boost::interprocess::read_write),
+						coord(coordFile, boost::interprocess::read_write),
+						invCoord(invCoordFile, boost::interprocess::read_write),
+						mappedDict(dict, boost::interprocess::read_write),
+						mappedCoord(coord, boost::interprocess::read_write),
+						mappedInvCoord(invCoord, boost::interprocess::read_write) { }
 
-										  }
-
-template class Index<DefaultIndex::NodeType, DefaultIndex::DictNodeSize, DefaultIndex::CoordNodeSize>;
+template class Index<DefaultIndex>;
--- include/index.h
+++ include/index.h
@@ -6,21 +6,49 @@
 #include "index_iterator.h"
 
 
-struct DefaultIndex {
-	static constexpr size_t DictNodeSize = 3 * sizeof(size_t);
-	static constexpr size_t CoordNodeSize = 2 * sizeof(size_t);
+struct DefaultIndex {};
 
-	using NodeType = size_t;
+
+template<typename IndexType> struct IndexTraits { };
+template<> 
+struct IndexTraits<DefaultIndex> { 
+
+	struct Dictionary {
+		using Type = size_t;
+		static constexpr size_t NodeSize = 3 * sizeof(size_t);
+
+		struct CoordOffset { 
+			using Type = size_t;
+			static constexpr size_t Offset =  1 * sizeof(size_t);
+		};
+		struct Length { 
+			using Type = size_t;
+			static constexpr size_t Offset = 2 * sizeof(size_t);
+		};
+	};
+
+
+	struct CoordinateFile { 
+		using Type = size_t;
+		static constexpr size_t NodeSize = 2 * sizeof(size_t);
+
+		struct Position {
+			using Type = size_t;
+			static constexpr size_t Offset = 1 * sizeof(size_t);
+		};
+	};
 };
 
 
-template<typename NodeType=typename DefaultIndex::NodeType,
-		 size_t DictNodeSize=DefaultIndex::DictNodeSize,
- 		 size_t CoordNodeSize=DefaultIndex::CoordNodeSize>
+
+template<typename IndexType = DefaultIndex>
 class Index {
 public:
-	using DictionaryIterator = IndexIterator<NodeType, DictNodeSize>;
-	using CoordinateBlocksIterator = IndexIterator<NodeType, CoordNodeSize>;
+	using DictionaryIterator = IndexIterator<typename IndexTraits<IndexType>::Dictionary::Type, 
+											 IndexTraits<IndexType>::Dictionary::NodeSize>;
+											 
+	using CoordinateBlocksIterator = IndexIterator<typename IndexTraits<IndexType>::CoordinateFile::Type,
+												   IndexTraits<IndexType>::CoordinateFile::NodeSize>;
 
 	Index() = delete;
 	Index(const Index&) = delete;
