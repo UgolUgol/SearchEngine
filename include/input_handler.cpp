ccade4186b47809bb3d4005affdd9b7b427aae78
UgolUgol <trubchenkon@gmail.com>
2019-07-14 21:17:46 +0300

    use stemmer morphology for index creating

--- include/input_handler.cpp
+++ include/input_handler.cpp
@@ -3,6 +3,9 @@
 #include <tools.h>
 #include "reader.h"
 #include <iostream>
+#include <stemmer.h>
+
+
 namespace InputHandler {
 
 Output::DocId StandartHandler::docId = 1;
@@ -10,13 +13,19 @@ Output::DocId StandartHandler::docId = 1;
 template<typename Input>
 StandartHandler::OutputType StandartHandler::prepareForSort(Input&& input) {
 
+    using NameType = typename Input::Traits::NameType;
+    using UrlType = typename Input::Traits::UrlType;
+    using TokensType = typename Input::Traits::TokensType;
+
 	OutputType output;
 
 	OutputType::PositionType position = 1;
-	std::vector<typename Input::Traits::TokensType> tokens;
-	typename Input::Traits::NameType articleName = std::move(std::get<Input::Traits::Name>(input.data));
-	typename Input::Traits::UrlType url = std::move(std::get<Input::Traits::Url>(input.data));
-	typename Input::Traits::TokensType text = std::move(std::get<Input::Traits::Tokens>(input.data));
+	std::vector<TokensType> tokens;
+
+	NameType articleName = std::move(std::get<Input::Traits::Name>(input.data));
+	UrlType url = std::move(std::get<Input::Traits::Url>(input.data));
+	TokensType text = std::move(std::get<Input::Traits::Tokens>(input.data));
+
 
 	Tools::split_regex(tokens, text.c_str(), Input::separator, text.size());
 	for(auto& token : tokens) {
@@ -27,12 +36,14 @@ StandartHandler::OutputType StandartHandler::prepareForSort(Input&& input) {
 			continue;
 		}
 
-		output.data.emplace_back(std::hash<typename Input::Traits::TokensType>{}(token), 
+        Stemmer<typename TokensType::value_type> st(token);
+		output.data.emplace_back(std::hash<TokensType>{}(static_cast<TokensType>(st.normalize())),
 								 docId,
 								 articleName,
 								 url,
 								 position++);
 	}
+
 	if(!output.data.empty()) {
 		++docId;
 	}
